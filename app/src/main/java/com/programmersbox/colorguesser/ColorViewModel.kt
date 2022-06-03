19b51e14a24af45e7a3763e74cffa111d58fa195
Jacob Rein <jrein@phunware.com>
2022-06-03 07:44:13 -0400

    Initial commit

---
+++ app/src/main/java/com/programmersbox/colorguesser/ColorViewModel.kt
@@ -0,0 +1,206 @@
+package com.programmersbox.colorguesser
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+import androidx.lifecycle.ViewModel
+import kotlin.math.min
+import kotlin.random.Random
+
+private const val RGB_MAX = 255
+private const val CMYK_MAX = 100
+
+enum class GameState { Play, Restart }
+
+class ColorViewModel : ViewModel() {
+
+    var state by mutableStateOf(GameState.Play)
+
+    var color by mutableStateOf(Random.nextColor(a = 255))
+    var currentScore by mutableStateOf(0)
+
+    var hexValue by mutableStateOf("")
+
+    var rValue by mutableStateOf(0)
+    var gValue by mutableStateOf(0)
+    var bValue by mutableStateOf(0)
+
+    var cValue by mutableStateOf(0)
+    var mValue by mutableStateOf(0)
+    var yValue by mutableStateOf(0)
+    var kValue by mutableStateOf(0)
+
+    fun guess() {
+        var addedScore = 0
+
+        val color: Int = this.color.toArgb()
+
+        //------RGB-----
+        addedScore += rgbScore(color)
+
+        //----HEX----
+        addedScore += hexScore(color)
+
+        //-----CMYK-----
+        addedScore += cmykScore(color)
+
+        currentScore += addedScore
+
+        state = GameState.Restart
+    }
+
+    private fun rgbScore(color: Int): Int {
+        val rGuessed: String = rValue.toString()
+        val gGuessed: String = gValue.toString()
+        val bGuessed: String = bValue.toString()
+        //if any of the fields are empty, no points...no matter what
+        //if any of the fields are empty, no points...no matter what
+        return if (rGuessed.isNotEmpty() && gGuessed.isNotEmpty() && bGuessed.isNotEmpty()) {
+            val r = rGuessed.toInt()
+            val g = gGuessed.toInt()
+            val b = bGuessed.toInt()
+            val rScore: Int = getScore(color.red, r)
+            val gScore: Int = getScore(color.green, g)
+            val bScore: Int = getScore(color.blue, b)
+
+            //if all three are correct, CONFETTI!!!
+            if (rScore == RGB_MAX && gScore == RGB_MAX && bScore == RGB_MAX) {
+                //confetti(Color.RED, Color.GREEN, Color.BLUE)
+            }
+            rScore + gScore + bScore
+        } else 0
+    }
+
+    private fun hexScore(color: Int): Int {
+        val hexGuess: String = hexValue
+
+        return try {
+            //if the hex field is empty, ZERO points!
+            if (hexGuess != "") {
+                //parse the color
+                val c: Int = android.graphics.Color.parseColor("#$hexGuess")
+                //get the rgb values for it
+                val r = c shr 16 and 0xff
+                val g = c shr 8 and 0xff
+                val b = c and 0xff
+                val rScore: Int = getScore(color.red, r)
+                val gScore: Int = getScore(color.green, g)
+                val bScore: Int = getScore(color.blue, b)
+                //if its perfect, CONFETTI!
+                if (rScore == RGB_MAX && gScore == RGB_MAX && bScore == RGB_MAX) {
+                    //confetti(Color.RED, Color.GREEN, Color.BLUE)
+                }
+                rScore + gScore + bScore
+            } else 0
+        } catch (e: NumberFormatException) {
+            0
+        }
+    }
+
+    private fun cmykScore(color: Int): Int {
+        val cGuessed: String = cValue.toString()
+        val mGuessed: String = mValue.toString()
+        val yGuessed: String = yValue.toString()
+        val kGuessed: String = kValue.toString()
+        //this is all to get the CMYK values. Taken from online.
+        //http://www.javascripter.net/faq/hex2cmyk.htm
+        //this is all to get the CMYK values. Taken from online.
+        //http://www.javascripter.net/faq/hex2cmyk.htm
+        var computedC = 1 - (color.red.toString() + "").toDouble() / RGB_MAX
+        var computedM = 1 - (color.green.toString() + "").toDouble() / RGB_MAX
+        var computedY = 1 - (color.blue.toString() + "").toDouble() / RGB_MAX
+
+        val minCMY = min(computedC, min(computedM, computedY))
+
+        computedC = (computedC - minCMY) / (1 - minCMY)
+        computedM = (computedM - minCMY) / (1 - minCMY)
+        computedY = (computedY - minCMY) / (1 - minCMY)
+        //the CMYK values
+        //the CMYK values
+        val actualC = (computedC * 100)
+        val actualM = (computedM * 100)
+        val actualY = (computedY * 100)
+        val actualK = (minCMY * 100)
+        //if any of the fields are empty, no points
+        //if any of the fields are empty, no points
+        return if (cGuessed.isNotEmpty() && mGuessed.isNotEmpty() && yGuessed.isNotEmpty() && kGuessed.isNotEmpty()) {
+            val c = cGuessed.toDouble()
+            val m = mGuessed.toDouble()
+            val y = yGuessed.toDouble()
+            val k = kGuessed.toDouble()
+            //making sure everything is within the right values
+            if ((c <= 100 || c >= 0) && (m <= 100 || m >= 0) && (y <= 100 || y >= 0) && (k <= 100 || k >= 0)) {
+                val cs: Double = getCMYKScore(actualC, c)
+                val ms: Double = getCMYKScore(actualM, m)
+                val ys: Double = getCMYKScore(actualY, y)
+                val ks: Double = getCMYKScore(actualK, k)
+                //if everything is perfect, CONFETTI!
+                if (cs == CMYK_MAX.toDouble() && ms == CMYK_MAX.toDouble() && ys == CMYK_MAX.toDouble() && ks == CMYK_MAX.toDouble()) {
+
+                }
+                (cs + ms + ys + ks).toInt()
+            } else {
+                0
+            }
+        } else 0
+    }
+
+    fun reset() {
+        hexValue = ""
+        rValue = 0
+        gValue = 0
+        bValue = 0
+        cValue = 0
+        mValue = 0
+        yValue = 0
+        kValue = 0
+        state = GameState.Play
+    }
+}
+
+/**
+ * getScore - gets the score
+ * @param actual - the actual value
+ * @param guessed - the guessed value
+ * @return - points
+ */
+fun getScore(actual: Int, guessed: Int): Int {
+    //RGB_MAX - difference
+    return RGB_MAX - (if (actual >= guessed) actual - guessed else guessed - actual).coerceIn(0, 255)
+}
+
+/**
+ * getCMYKScore - gets the score
+ * @param actual - the actual value
+ * @param guessed - the guessed value
+ * @return - points
+ */
+fun getCMYKScore(actual: Double, guessed: Double): Double {
+    //CMYK_MAX - difference
+    return CMYK_MAX - (if (actual >= guessed) actual - guessed else guessed - actual).coerceIn(0.0, 100.0)
+}
+
+data class CMYK(val c: Double, val m: Double, val y: Double, val k: Double)
+
+fun getCMYKFromRGB(r: Int, g: Int, b: Int): CMYK {
+    var computedC: Double = 1 - (r.toString() + "").toDouble() / RGB_MAX
+    var computedM: Double = 1 - (g.toString() + "").toDouble() / RGB_MAX
+    var computedY: Double = 1 - (b.toString() + "").toDouble() / RGB_MAX
+
+    val minCMY = min(computedC, min(computedM, computedY))
+
+    computedC = (computedC - minCMY) / (1 - minCMY)
+    computedM = (computedM - minCMY) / (1 - minCMY)
+    computedY = (computedY - minCMY) / (1 - minCMY)
+    var computedK = minCMY
+
+    computedC = String.format("%.3f", computedC).toDouble()
+    computedM = String.format("%.3f", computedM).toDouble()
+    computedY = String.format("%.3f", computedY).toDouble()
+    computedK = String.format("%.3f", computedK).toDouble()
+    return CMYK(computedC, computedM, computedY, computedK)
+}
---
+++ app/src/main/java/com/programmersbox/colorguesser/MainActivity.kt
@@ -0,0 +1,268 @@
+package com.programmersbox.colorguesser
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.programmersbox.colorguesser.ui.theme.ColorGuesserTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            val vm: ColorViewModel = viewModel()
+            val animateColor by animateColorAsState(targetValue = vm.color)
+            
+            ColorGuesserTheme(
+                customPrimaryColor = animateColor
+            ) {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) { ColorGuesserView(vm) }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ColorGuesserView(vm: ColorViewModel = viewModel()) {
+
+    val animateColor by animateColorAsState(targetValue = vm.color)
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                icons = {
+                    Text("Score: ${animateIntAsState(vm.currentScore).value}")
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = { vm.color = Random.nextColor(a = 255) },
+                        containerColor = MaterialTheme.colorScheme.primary
+                    ) { Icon(Icons.Default.Refresh, null) }
+                }
+            )
+        }
+    ) { p ->
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(p)
+                .padding(horizontal = 4.dp)
+        ) {
+            val (circle, colorInfo, guessArea) = createRefs()
+
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary, CircleShape)
+                    .border(2.dp, Color.Black, CircleShape)
+                    .constrainAs(circle) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(colorInfo.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            )
+
+            AnimatedVisibility(visible = vm.state == GameState.Restart) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .constrainAs(colorInfo) {
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            top.linkTo(circle.bottom)
+                            bottom.linkTo(guessArea.top)
+                        }
+                ) {
+                    Column(
+                        modifier = Modifier.padding(14.dp),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        val rgb = animateColor.toArgb()
+                        Text("RGB: Red = ${rgb.red}, Green = ${rgb.green}, Blue = ${rgb.blue}")
+                        Text("Hex: ${Integer.toHexString(rgb).drop(2)}")
+                        val cmyk = getCMYKFromRGB(rgb.red, rgb.green, rgb.blue)
+                        Text("CMYK: C = ${(cmyk.c * 100).toInt()}, M = ${(cmyk.m * 100).toInt()}, Y = ${(cmyk.y * 100).toInt()}, K = ${(cmyk.k * 100).toInt()}")
+                    }
+                }
+            }
+
+            Column(
+                modifier = Modifier.constrainAs(guessArea) {
+                    top.linkTo(colorInfo.bottom)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            ) {
+
+                OutlinedButton(
+                    onClick = { vm.guess() },
+                    modifier = Modifier.fillMaxWidth(),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                    ),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colorScheme.onPrimaryContainer)
+                ) { Text("Make a Guess") }
+
+                OutlinedTextField(
+                    value = vm.hexValue,
+                    onValueChange = { vm.hexValue = it },
+                    leadingIcon = { Text("#") },
+                    label = { Text("Hex Color") },
+                    singleLine = true
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        "RGB",
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    NumberOutlinedTextField(
+                        value = vm.rValue.toString(),
+                        onValueChange = { vm.rValue = it.toIntOrNull()?.coerceIn(0, 255) ?: 0 },
+                        label = "R",
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    NumberOutlinedTextField(
+                        value = vm.gValue.toString(),
+                        onValueChange = { vm.gValue = it.toIntOrNull()?.coerceIn(0, 255) ?: 0 },
+                        label = "G",
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    NumberOutlinedTextField(
+                        value = vm.bValue.toString(),
+                        onValueChange = { vm.bValue = it.toIntOrNull()?.coerceIn(0, 255) ?: 0 },
+                        label = "B",
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        "CMYK",
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    NumberOutlinedTextField(
+                        value = vm.cValue.toString(),
+                        onValueChange = { vm.cValue = it.toIntOrNull()?.coerceIn(0, 100) ?: 0 },
+                        label = "C",
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    NumberOutlinedTextField(
+                        value = vm.mValue.toString(),
+                        onValueChange = { vm.mValue = it.toIntOrNull()?.coerceIn(0, 100) ?: 0 },
+                        label = "M",
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    NumberOutlinedTextField(
+                        value = vm.yValue.toString(),
+                        onValueChange = { vm.yValue = it.toIntOrNull()?.coerceIn(0, 100) ?: 0 },
+                        label = "Y",
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    NumberOutlinedTextField(
+                        value = vm.kValue.toString(),
+                        onValueChange = { vm.kValue = it.toIntOrNull()?.coerceIn(0, 100) ?: 0 },
+                        label = "K",
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NumberOutlinedTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
+}
+
+fun Random.nextColor(
+    r: Int = nextInt(0, 255),
+    g: Int = nextInt(0, 255),
+    b: Int = nextInt(0, 255),
+    a: Int = nextInt(0, 255)
+) = Color(r, g, b, a)
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultPreview() {
+    ColorGuesserTheme {
+        ColorGuesserView()
+    }
+}
